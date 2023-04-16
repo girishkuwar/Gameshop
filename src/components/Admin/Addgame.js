3ca7fd0799faf1503171496a31c8868e3ecab47a
girishkuwar <girishkuwar11@gmail.com>
2023-04-16 10:50:28 +0530

    admin

--- src/components/Admin/Addgame.js
+++ src/components/Admin/Addgame.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Addgame = () => {
   return (
-    <div>
-      <input type="text" />
+    <div className='addgame'>
+      <h5>Game Cover</h5>
+      <input type="file" src="" alt="" />
+      <h5>Name</h5>
+      <input type="text" placeholder='name'/>
+      <h5>Description</h5>
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+      <h5>Price</h5>
+      <input type="number" />
+      <h5>Quantity</h5>
+      <input type="number" name="" id="" />
     </div>
   )
 }
--- src/components/Admin/GameList.js
+++ src/components/Admin/GameList.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import gamelist from '../../data/GamesList'
 
 const GameList = () => {
   return (
-    <div>
-      
+    <div className='list'>
+      {
+        gamelist.map((g) => {
+          return (<div className='item'>
+            <img src={g.img} alt="" />
+            <h5>{g.name}</h5>
+            <button>Update</button>
+            <button>Delete</button>
+            </div>)
+        })
+      }
     </div>
   )
 }
