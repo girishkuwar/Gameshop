2ed6cb958cc391f511976367270c814cee88afcd
girishkuwar <girishkuwar11@gmail.com>
2023-04-16 09:33:55 +0530

    ok

---
+++ src/components/Admin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div className='inp'>
+      <input type="text" placeholder='name'/>
+      <input type="text" placeholder='desc'/>
+      <button>Submit</button>
+    </div>
+  )
+}
+
+export default Admin
---
+++ src/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALbVd_O1lUm_BAMKyHy0sOW0udWcO7qQk",
+  authDomain: "gameshop-610be.firebaseapp.com",
+  databaseURL: "https://gameshop-610be-default-rtdb.firebaseio.com",
+  projectId: "gameshop-610be",
+  storageBucket: "gameshop-610be.appspot.com",
+  messagingSenderId: "86322725856",
+  appId: "1:86322725856:web:2551ce37eca84442c5a185"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
