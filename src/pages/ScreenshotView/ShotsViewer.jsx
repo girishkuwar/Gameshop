ebeda51fa1adc54ac81969763919d84119bd8282
Girish Kuwar <109171713+girishkuwar@users.noreply.github.com>
2025-10-22 19:43:28 +0530

    id added

--- src/pages/ScreenshotView/ShotsViewer.jsx
+++ src/pages/ScreenshotView/ShotsViewer.jsx
@@ -7,7 +7,7 @@ const ShotsViewer = ({ display, gamename, id, close ,gameid}) => {
     const [imgno, setimgno] = useState();
     const [isIdlikeImg, setIsIdlikeImg] = useState(false);
     // const [img, setimg] = useState(`https://tfnokgublfaoehupzhtc.supabase.co/storage/v1/object/public/gamespics/public/${gamename}/${imgno}.jpg`);
-    const [img, setimg] = useState(`https://kftaoqxgwyzsujayhbca.supabase.co/storage/v1/object/public/gamespics/public/${gameid}/${i}.jpg`);
+    const [img, setimg] = useState(`https://kftaoqxgwyzsujayhbca.supabase.co/storage/v1/object/public/gamespics/public/${gameid}/${id}.jpg`);
     
 
 
